d2e4dfc2240abfc7cf60c9e0290210d3ae2b06ea
Artur Abdalimov <a.abdalimov.97@gmail.com>
2018-02-22 14:13:26 +0600

    Add composition structure image

--- front-end/src/components/card-board/board/index.js
+++ front-end/src/components/card-board/board/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 class Board extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cards: []
+        }
+    }
+
     render() {
         return(
             <div>

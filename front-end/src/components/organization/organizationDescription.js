d86f8e5c1b310e8bd603d912f064e7379936b42f
Mariyam <msisengali@gmail.com>
2018-03-01 00:53:14 +0600

    organization page changes

--- front-end/src/components/organization/organizationDescription.js
+++ front-end/src/components/organization/organizationDescription.js
@@ -10,7 +10,14 @@ class organizationDescription extends Component{
           <h1 class="jumbotron-heading">Organization</h1>
           <p class="lead text-muted">Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
           <p>
-            
+          <div class = "description-component">
+            <label> Address:</label>
+            <p class="lead">Seifullin 535 </p>
+          </div>
+          <div class = "description-component">
+            <label> Telephone:</label>
+            <p class="lead">34-556-78</p>
+          </div>
           </p>
         </div>
       </section>

e8aca9a4a7c97539febdda564203e514f6bdd348
Mariyam <msisengali@gmail.com>
2018-02-28 22:39:46 +0600

    registation form added

--- front-end/src/components/Registration/RegistrationModal.js
+++ front-end/src/components/Registration/RegistrationModal.js
@@ -7,16 +7,24 @@ class RegistrationModal extends Component{
       return(
 
         <div class='modal'>
-
           <div class = 'modal-body'>
             <div class="container">
               <span class="close">&times;</span>
               <form class="form-signin">
                 <h2 class="form-signin-heading">Register</h2>
+
+                <label for="inputName" class="sr-only">Name</label>
+                <input type="name" id="inputName" class="form-control" placeholder="Name" required="" autofocus=""/>
+
+                <label for="inputSurname" class="sr-only">Surname</label>
+                <input type="name" id="inputSurname" class="form-control" placeholder="Surname" required="" autofocus=""/>
+
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""/>
+
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""/>
+
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
               </form>
             </div>

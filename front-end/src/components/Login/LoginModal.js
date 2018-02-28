8582baab54e9c5e8e5e3fb12e64b1f1a532cc081
Mariyam <msisengali@gmail.com>
2018-02-28 22:41:46 +0600

    login form added

--- front-end/src/components/Login/LoginModal.js
+++ front-end/src/components/Login/LoginModal.js
@@ -3,7 +3,7 @@ import './styles.css';
 
 class LoginModal extends Component{
   render() {
-    //if(this.props.isRegistration){
+    //if(this.props.isLogin){
       return(
 
         <div class='modal'>
@@ -11,13 +11,7 @@ class LoginModal extends Component{
             <div class="container">
               <span class="close">&times;</span>
               <form class="form-signin">
-                <h2 class="form-signin-heading">Register</h2>
-
-                <label for="inputName" class="sr-only">Name</label>
-                <input type="name" id="inputName" class="form-control" placeholder="Name" required="" autofocus=""/>
-
-                <label for="inputSurname" class="sr-only">Surname</label>
-                <input type="name" id="inputSurname" class="form-control" placeholder="Surname" required="" autofocus=""/>
+                <h2 class="form-signin-heading">Login</h2>
 
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""/>

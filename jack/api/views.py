c31712661c12ef32afca62732a8b40e0f636c5a4
MarcyMARLY <msisenga>
2018-04-21 03:26:50 +0600

    update views

--- jack/api/views.py
+++ jack/api/views.py
@@ -11,10 +11,37 @@ from .serializers import OrganizationSerializer,CardSerializer,GatherSerializer
 # Create your views here.
 @csrf_exempt
 def organizations(request):
+    if request.method == "GET":
+        organizations = Organization.objects.all
+        ser = OrganizationSerializer(organizations, many = True)
+        return JsonResponse(ser.data, safe = False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        ser = OrganizationSerializer(data=data)
+        if(ser.is_valid()):
+            ser.save()
+            return JsonResponse(ser.data,status=201)
+        return JsonResponse(ser.errors, status=400)
 
 @csrf_exempt
 def organization_detail(request, or_id):
-
+    try:
+        organization = Organization.objects.get(pk=or_id)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=404)
+    if request.method == "GET":
+        ser = OrganizationSerializer(organization)
+        return JsonResponse(ser.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        ser = OrganizationSerializer(organization, data)
+        if(ser.is_valid()):
+            ser.save()
+            return JsonResponse(ser.data)
+    elif request.method == "DELETE":
+        organization.delete()
+        ser = OrganizationSerializer(organization)
+        return JsonResponse(ser.data) 
 @csrf_exempt
 def gathers(request):
 

9640720bd9e2da7dea40d270f680b51060ee6736
AbdalimovArtur <a.abdalimov.97@gmail.com>
2018-04-21 12:10:30 +0600

    Add delete gather button

--- front-end/src/components/card-gather/gather/index.js
+++ front-end/src/components/card-gather/gather/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import './style.css'
 import CardContainer from "../../card-container";
+import {MdDeleteForever} from "react-icons/lib/md/index";
 
 class CardGather extends React.Component {
+
+    deleteGather = () => {
+        console.log(this.props.id)
+    };
 
     render() {
         return (
             <div className="card-gather container">
                 <h1 className="gather-header">{this.props.title}</h1>
-                <hr />
-                <CardContainer cards={this.props.cards} gatherId={this.props.id} onCreateCard={this.props.onCreateCard} />
+                <div onClick={this.deleteGather} className="gather-delete">
+                    <MdDeleteForever size={50}/>
+                </div>
+                <hr/>
+                <CardContainer cards={this.props.cards} gatherId={this.props.id}
+                               onCreateCard={this.props.onCreateCard}/>
             </div>
         )
     }
